a6a092c76f7b2d7c2388375ad805f3068b667414
Shuga8 <lotocharles8@gmail.com>
2025-06-11 12:38:52 +0100

    creating new data

---
+++ services/appwrite.ts
@@ -0,0 +1,38 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECT_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PUBLIC_ID!);
+
+const database = new Databases(client);
+
+// track searches made by a user
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECT_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existinMovie = result.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECT_ID, existinMovie.$id, {
+        count: existinMovie.count + 1,
+      });
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECT_ID, ID.unique(), {
+        searchTerm: query,
+        movie_id: movie.id,
+        count: 1,
+        title: movie.title,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (e: Error | any) {
+    console.log(e);
+    throw e;
+  }
+};
